91f980bb1b5b00ceb02b3f3b38006f4c50656f17
Son YeongJun <sonyj97@gmail.com>
2018-04-07 21:12:06 +0900

    second commit

--- Chapter 6/exercise_ch6.2_4.py
+++ Chapter 6/exercise_ch6.2_4.py
@@ -35,3 +35,75 @@ initials('Vladimir "Pootie-Poot" Putin')
 
 initials('Georges Perec')
 
+
+# In[5]:
+
+
+def initials_2(name):
+    list = name.split(" ")
+    capital = ''
+    
+    for i in range(len(list)):
+        capital = capital + str(list[i])[0]
+        
+    return capital
+
+
+# In[6]:
+
+
+initials_2('Georges Perec')
+
+
+# In[67]:
+
+
+def initials_3(name):
+    list_old = name.split(" ")
+    
+    
+    for i in range(len(list_old)):
+        list_new = []
+        list_new.append(str(list_old[i])[0])
+    
+    return "".join(list_new)
+
+## list_new가 반복문 안에 있을 경우 리셋됨.
+
+
+# In[68]:
+
+
+initials_3('International Business Machines')
+
+
+# In[61]:
+
+
+def initials_3(name):
+    list_old = name.split(" ")
+    list_new = []
+    
+    for i in range(len(list_old)):    
+        list_new.append(str(list_old[i])[0])
+    
+    return "".join(list_new)
+
+
+# In[66]:
+
+
+initials_3('International Business Machines')
+
+
+# In[64]:
+
+
+initials_3('Vladimir "Pootie-Poot" Putin')
+
+
+# In[65]:
+
+
+initials_3('Georges Perec')
+
